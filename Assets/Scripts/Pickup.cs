12b793938936f94036338f957d450b2eeba2eddb
AIE\s167697 <dmarton94@gmail.com>
2018-03-06 16:35:43 +1100

    - Pickup objects script now spawns based off a timer

--- Assets/Scripts/Pickup.cs
+++ Assets/Scripts/Pickup.cs
@@ -8,51 +8,73 @@ public class Pickup : MonoBehaviour {
 
     public enum EPickupType { Shield, SpeedBoost, Minigun, LaserBeam, Count_DONT_USE_EVEN_IF_THERES_A_FIRE }
     public EPickupType _PickupType;
+    public float _SpawnTime = 0f;
+    private float _SpawnTimer = 0f;
+    private bool _Enabled = false;
+    private MeshRenderer _Renderer;
+
+    private void Start() {
+
+        // Get component references
+        _Renderer = GetComponent<MeshRenderer>();
+    }
+
+    private void Update() {
+
+        // Used for spawning
+        _Enabled = _SpawnTimer >= _SpawnTime;
+        _SpawnTimer += Time.deltaTime;
+
+        _Renderer.enabled = _Enabled;
+    }
 
     private void OnTriggerEnter(Collider other) {
-        
-        if (other.gameObject.tag == "Destroyable") {
 
-            TankController tank = other.gameObject.GetComponent<TankController>();
-            if (tank) {
+        if (_Enabled) {
 
-                // Apply pickup modifier based on type
-                switch (_PickupType) {
+            if (other.gameObject.tag == "Destroyable") {
 
-                    case EPickupType.Shield: {
+                TankController tank = other.gameObject.GetComponent<TankController>();
+                if (tank) {
 
-                        // Shield
-                        tank.SetHasShield(true);
-                        break;
+                    // Apply pickup modifier based on type
+                    switch (_PickupType) {
+
+                        case EPickupType.Shield: {
+
+                                // Shield
+                                tank.SetHasShield(true);
+                                break;
+                            }
+
+                        case EPickupType.SpeedBoost: {
+
+                                // Speed boost
+                                tank.SetSpeedModifier(MatchManager._pInstance._SpeedBoostModifier);
+                                break;
+                            }
+
+                        case EPickupType.Minigun: {
+
+                                // Minigun
+                                tank.SetFiremode(TankController.EFiremode.FullAuto);
+                                break;
+                            }
+
+                        case EPickupType.LaserBeam: {
+
+                                // Laser beam
+                                tank.SetFiremode(TankController.EFiremode.Charge);
+                                break;
+                            }
+
+                        default: break;
                     }
-
-                    case EPickupType.SpeedBoost: {
-
-                        // Speed boost
-                        tank.SetSpeedModifier(MatchManager._pInstance._SpeedBoostModifier);
-                        break;
-                    }
-
-                    case EPickupType.Minigun: {
-
-                        // Minigun
-                        tank.SetFiremode(TankController.EFiremode.FullAuto);
-                        break;
-                    }
-
-                    case EPickupType.LaserBeam: {
-
-                        // Laser beam
-                        tank.SetFiremode(TankController.EFiremode.Charge);
-                        break;
-                    }
-
-                    default: break;
                 }
             }
-        }
 
-        // Destroy pickup object
-        Destroy(gameObject);
+            // Destroy pickup object
+            Destroy(gameObject);
+        }
     }
 }
